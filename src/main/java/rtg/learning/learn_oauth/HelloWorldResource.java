617829b5206dc84f46554e7394ea89d0bde82340
me-coderaider <kashyappankajnsp@gmail.com>
2024-08-04 11:03:33 +0530

    Learn OAuth

---
+++ src/main/java/rtg/learning/learn_oauth/HelloWorldResource.java
@@ -0,0 +1,16 @@
+package rtg.learning.learn_oauth;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldResource {
+
+	@GetMapping(path="/")
+	public String helloWorld(Authentication authentication) {
+		System.out.println(authentication);
+		System.out.println(authentication.getPrincipal());
+		return "Be Creative";
+	}
+}
---
+++ src/main/java/rtg/learning/learn_oauth/OAuthSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package rtg.learning.learn_oauth;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class OAuthSecurityConfiguration {
+	@Bean
+	@Order(SecurityProperties.BASIC_AUTH_ORDER)
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+//		http.formLogin(withDefaults());
+//		http.httpBasic(withDefaults());
+		http.oauth2Login(withDefaults());
+		return http.build();
+	}
+}
